0c593d0702de3d09687e3760631095570b34243f
Jyoti-246 <jyotikedia203@gmail.com>
2024-12-18 13:54:16 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,48 @@
+const name = "jyoti";
+const repoCount = 50;
+
+// console.log(name + repoCount + " value");
+
+// console.log(`hello my name is ${name} and my repo Count is ${repoCount}`);
+
+const gameName = new String('jyoti-kedia-2024')
+
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(1,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-5, 3)
+// console.log(anotherString);
+
+// const newStringOne = "  jyoti    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('hitesh'));
+// console.log(url.includes('kedia'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -7,7 +7,7 @@ const score = 100
 let id = Symbol('123')
 let anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 let bigNumber = 345678903456789n;
 
@@ -27,10 +27,38 @@ const myFunction = function(){
     console.log("hello world");
 }
 
-console.log(typeof bigNumber);
-console.log(typeof myFunction);
-console.log(typeof myObj);
-console.log(typeof heros);
+// console.log(typeof bigNumber);
+// console.log(typeof myFunction);
+// console.log(typeof myObj);
+// console.log(typeof heros);
+
+
+// ****************************************************************************
+
+// stack (primitive), heap (non-primitive)
+
+let myYoutubeName = "pooja kedia"
+
+let anotherName = "lokesh kedia"
+anotherName = "chaiAurCode"
+
+console.log(anotherName);
+console.log(myYoutubeName);
+
+let userOne = {
+    email: "jyoti@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "pooja@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
 
 
 
