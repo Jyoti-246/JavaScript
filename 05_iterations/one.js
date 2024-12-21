4ef82ea8e9b7fb8b153ccca3e474d33c225a1a9f
Jyoti-246 <jyotikedia203@gmail.com>
2024-12-21 05:00:32 +0000

    loops

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -6,9 +6,29 @@
 const arr = [1,2,3,4,5,6]
 
 for (const num of arr) {
-    console.log(num);
+    // console.log(num);
 }
 
 
 // Maps
 
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);  
+}
+
+// const myObj = {
+//     game1: "NFS",
+//     game2: "Spiderman"
+// }
+
+// for (const [key, value] of myObj) {
+//     console.log(key, "=", value);
+// }
